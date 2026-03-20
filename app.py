4400d1c1b1e463e88548acf8a5a013fe0b222bf6
kumarsrigiribharath-lab <kumarsrigiribharath@gmail.com>
2026-03-20 18:52:02 +0530

    Refactor app.py for improved functionality and style

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ body {
 """, unsafe_allow_html=True)
 
 try:
-    model = pickle.load(open("model.pkl", "rb"))
+    model = pickle.load(open("model.pkl", "wb"))
 except Exception as e:
     st.error(f"Model loading error:{e}")
 st.title("🎓 Student Exam Score Predictor")
